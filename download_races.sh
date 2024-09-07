c838407c8104b8466eac93dd843b0e753711f4c6
jxu <7989982+jxu@users.noreply.github.com>
2024-09-07 13:44:32 -0400

    Shorten download races curl with tee

--- download_races.sh
+++ download_races.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 mkdir races
-curl https://data.ninjakiwi.com/btd6/races > races/races$(date +%Y%m%d).json
-
-curl https://data.ninjakiwi.com/btd6/races |
+curl https://data.ninjakiwi.com/btd6/races | tee races/races$(date +%Y%m%d).json |
 jq -r '.body[] | .leaderboard, .metadata | "url = "+., "output = "+((./"/")[-3:] | join("/"))+".json"' |
 curl -Z -K - --create-dirs
