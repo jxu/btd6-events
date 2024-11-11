8e612d552abfd754b8a1245e2b4df89fceb42f7a
jxu <7989982+jxu@users.noreply.github.com>
2024-11-11 16:23:41 -0500

    Update download script to not complain on existing dir

--- download_races.sh
+++ download_races.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir races
+mkdir -p races  # mkdir if not exist
 curl https://data.ninjakiwi.com/btd6/races | tee races/races$(date +%Y%m%d).json |
 jq -r '.body[] | .leaderboard, .metadata | "url = "+., "output = "+((./"/")[-3:] | join("/"))+".json"' |
 curl -Z -K - --create-dirs
