3f58901535993b4f610b084fe7de024917c52601
jxu <7989982+jxu@users.noreply.github.com>
2025-09-16 15:05:56 -0400

    Add boss elite metadata

--- download_bosses.sh
+++ download_bosses.sh
@@ -2,6 +2,6 @@
 mkdir -p bosses
 curl https://data.ninjakiwi.com/btd6/bosses |
 tee bosses/bosses$(date +%Y%m%d).json |
-jq -r '.body[] | .leaderboard_standard_players_1, .leaderboard_elite_players_1, .metadataStandard | 
+jq -r '.body[] | .leaderboard_standard_players_1, .leaderboard_elite_players_1, .metadataStandard, .metadataElite | 
 "url = "+., "output = "+((./"/")[4:] | join("/"))+".json"' |
 curl -Z -K - --create-dirs
